d857f1ecdd1c052245674817bee3806bec7ce2dd
Your Name <you@example.com>
2024-12-23 09:51:55 +0000

    Add lots of Op and its verify() & infer()

--- examples/CodeGen/GenMixCode.cpp
+++ examples/CodeGen/GenMixCode.cpp
@@ -52,8 +52,8 @@ auto genSelfAttn(mlir::MLIRContext &context, mlir::OpBuilder &builder,
                  TypedValue<RankedTensorType> &residual,
                  TypedValue<RankedTensorType> &attention_mask) {
   auto hidden_statesType = hidden_states.getType();
-  // auto self_attn_output = builder.create<mix::SelfAttentionOp>(
-  //     loc, hidden_statesType, hidden_states, residual, attention_mask);
+  auto self_attn_output = builder.create<mix::SelfAttentionOp>(
+      loc, hidden_statesType, hidden_states, residual, attention_mask);
 
   /* 定义一些可重用的信息 */
   auto i32Type = builder.getI32Type();
@@ -63,10 +63,10 @@ auto genSelfAttn(mlir::MLIRContext &context, mlir::OpBuilder &builder,
   /* 定义算子 */
   SmallVector<Attribute> transpose1_dim{attr_i1, attr_i0};
   auto transpose1_dim_attr = mlir::ArrayAttr::get(&context, transpose1_dim);
-  auto transpose1 = builder.create<mix::PermuteOp>(
-      loc, hidden_statesType, hidden_states, transpose1_dim_attr);
+  // auto transpose1 = builder.create<mix::PermuteOp>(
+  //     loc, hidden_statesType, hidden_states, transpose1_dim_attr);
 
-  return transpose1;
+  return self_attn_output;
 }
 
 auto genRMSNorm(mlir::MLIRContext &context, mlir::OpBuilder &builder,
@@ -106,15 +106,18 @@ auto genTransformerBlock(mlir::MLIRContext &context, mlir::OpBuilder &builder,
 }
 
 auto genTelechatModel(mlir::MLIRContext &context, mlir::OpBuilder &builder,
-                      Location loc, int64_t seq_length, int64_t vocab_size) {
-  // 创建ml_program::subgraph
+                      Location loc, int64_t hidden_size, int64_t seq_length,
+                      int64_t vocab_size) {
+
+  /* Types */
+  auto F32Type = builder.getF32Type();
   auto input_ids_type =
       RankedTensorType::get({1, seq_length}, builder.getI32Type());
   auto output_type =
       RankedTensorType::get({seq_length, vocab_size}, builder.getF32Type());
-
   auto functionTy = builder.getFunctionType({input_ids_type}, {output_type});
 
+  // 创建ml_program::subgraph
   auto graph = builder.create<ml_program::SubgraphOp>(
       loc, "Telechat", functionTy, ArrayAttr{}, ArrayAttr{},
       builder.getStringAttr("private"));
@@ -123,10 +126,14 @@ auto genTelechatModel(mlir::MLIRContext &context, mlir::OpBuilder &builder,
   builder.setInsertionPointToEnd(body);
 
   auto Argument0 = graph.getArgument(0);
-  auto Argument1 = graph.getArgument(1);
+  auto input_ids = mlir::dyn_cast<TypedValue<RankedTensorType>>(Argument0);
 
-  auto hidden_states = mlir::dyn_cast<TypedValue<RankedTensorType>>(Argument0);
-  auto attention_mask = mlir::dyn_cast<TypedValue<RankedTensorType>>(Argument1);
+  /* 逻辑 */
+  auto input_embedding = builder.create<mix::LinearOp>(
+      loc, input_ids, "input_embedding.mlp.linear0", seq_length, hidden_size,
+      false, F32Type);
+
+  auto hidden_states = input_embedding.getOutput();
 
   int n_layer = 38;
   Operation *transformerBlock;
@@ -134,16 +141,19 @@ auto genTelechatModel(mlir::MLIRContext &context, mlir::OpBuilder &builder,
   for (int i = 0; i < n_layer; ++i) {
     // transformer block
     transformerBlock = genTransformerBlock(context, builder, graph->getLoc(),
-                                           hidden_states, attention_mask);
+                                           hidden_states, hidden_states);
 
     hidden_states = mlir::dyn_cast<mix::MLPOp>(transformerBlock).getOutput();
   }
   auto last_transformerBlock = mlir::dyn_cast<mix::MLPOp>(transformerBlock);
 
-  // TODO:加入Linear层映射过去
+  hidden_states = last_transformerBlock.getOutput();
+
+  // Linear:将hidden_states映射到vocab_size上
   auto output_embedding = builder.create<mix::LinearOp>(
-      last_transformerBlock->getLoc(), output_type,
-      last_transformerBlock.getOutput(), nullptr);
+      loc, hidden_states, "output_embedding.mlp.linear0", hidden_size,
+      vocab_size, false, F32Type);
+
   builder.create<ml_program::OutputOp>(loc, ValueRange{output_embedding});
   return graph;
 }
@@ -159,13 +169,15 @@ int main() {
   auto theModule = mlir::ModuleOp::create(loc);
   builder.setInsertionPointToEnd(theModule.getBody());
 
+  // hidden_size
+  int64_t hidden_size = 5120;
   // 输入token序列长度，静态长度
   int64_t seq_length = 40;
   // 字典长度
   int64_t vocab_size = 120000;
 
-  auto telechat_graph =
-      genTelechatModel(context, builder, loc, seq_length, vocab_size);
+  auto telechat_graph = genTelechatModel(context, builder, loc, hidden_size,
+                                         seq_length, vocab_size);
 
   theModule->dump();
 
