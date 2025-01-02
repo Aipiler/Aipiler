21667974d7149ad27f201acadd75d3e1f7288258
chh <cao18272922035@163.com>
2025-01-02 21:10:38 +0800

    Finish generating Telechat exe!

--- examples/CodeGen/print.cpp
+++ examples/CodeGen/print.cpp
@@ -8,13 +8,40 @@ template <typename T, int N> struct RankedMemRefType {
   int64_t strides[N];
 };
 extern "C" void _mlir_ciface_printMemrefF16(RankedMemRefType<uint16_t, 3> *in) {
+  int64_t shape[3];
   for (int64_t i = 0; i < 3; i++) {
+    shape[i] = in->sizes[i];
     printf("size[%ld] = %ld\n", i, in->sizes[i]);
   }
+  printf("[");
+  for (int64_t i = 0; i < shape[0]; i++) {
+    printf("[");
+    for (int64_t j = 0; j < shape[1]; j++) {
+      printf("[");
+      for (int64_t k = 0; k < shape[2]; k++) {
+        printf("%d, ", in->data[k + j * shape[2] + i * shape[2] * shape[1]]);
+        if (k > 64) {
+          printf("...");
+          break;
+        }
+      }
+      printf("], \n");
+      if (j > 64) {
+        printf("...");
+        break;
+      }
+    }
+    printf("], \n");
+    if (i > 64) {
+      printf("...");
+      break;
+    }
+  }
+  printf("]\n");
   return;
 }
 
 extern "C" void _mlir_ciface_printpoint() {
-  printf("point");
+  printf("point\n");
   return;
 }
