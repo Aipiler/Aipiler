93f967bee087ce428b1ade8ac3dc1db9e838736f
ShiHaoGao <1574987177@qq.com>
2025-06-04 02:13:58 +0000

    Add iree codegen test

--- test/test_iree.py
+++ test/test_iree.py
@@ -1,1 +1,44 @@
-import iree.runtime as ireert
+from typing import Tuple
+import functools
+from iree.compiler.dialects import func, memref, arith, builtin
+from iree.compiler.ir import *
+
+
+def run(f):
+    print("\nTEST:", f.__name__)
+    f()
+    return f
+
+
+# func.func()
+def gen_function() -> func.FuncOp:
+
+    arguments = []
+    shape1 = (3, ShapedType.get_dynamic_size())
+    f32 = F32Type.get()
+    shape2 = (ShapedType.get_dynamic_size(), 5)
+
+    arguments.append(RankedTensorType.get(shape1, f32))
+    arguments.append(RankedTensorType.get(shape2, f32))
+
+    results = []
+    shape3 = (3, 5)
+    results.append(RankedTensorType.get(shape3, f32))
+
+    function_type = FunctionType.get(inputs=arguments, results=results)
+
+    op = func.FuncOp(name="main", type=function_type, visibility="private")
+    return op
+
+
+@run
+def test():
+
+    with Context() as ctx, Location.unknown():
+
+        module = Module.create()
+
+        with InsertionPoint(module.body):
+
+            funcOp = gen_function()
+        print(module)
